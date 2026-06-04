54b207f01a8bbc87c9245f914334b10c44d194d9
Joe Tomlin <joetomlin8@gmail.com>
2026-06-03 17:30:05 -0700

    add numSet to reduce redeuncy

--- Arrays/longestConsecutiveSequence.py
+++ Arrays/longestConsecutiveSequence.py
@@ -6,7 +6,7 @@ class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
         numSet = set(nums)
         longest = 0
-        for n in nums:
+        for n in numSet:
             if n - 1 not in numSet:
                 length = 0
                 while n + length in numSet:
